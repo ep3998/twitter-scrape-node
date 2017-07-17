ccc222680d5e688bf003d04a7f708a71a06a96b0
Eric Pesch <ericpes@umich.edu>
2017-07-17 01:57:04 -0700

    First Commit - test application - Typescript + Express

---
+++ src/routes/index.ts
@@ -0,0 +1,64 @@
+/**
+ * Created by ep399 on 7/17/2017.
+ */
+import { NextFunction, Request, Response, Router } from "express";
+import { BaseRoute } from "./route";
+
+
+/**
+ * / route
+ *
+ * @class User
+ */
+export class IndexRoute extends BaseRoute {
+
+    /**
+     * Create the routes.
+     *
+     * @class IndexRoute
+     * @method create
+     * @static
+     */
+    public static create(router: Router) {
+        //log
+        console.log("[IndexRoute::create] Creating index route.");
+
+        //add home page route
+        router.get("/", (req: Request, res: Response, next: NextFunction) => {
+            new IndexRoute().index(req, res, next);
+        });
+    }
+
+    /**
+     * Constructor
+     *
+     * @class IndexRoute
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * The home page route.
+     *
+     * @class IndexRoute
+     * @method index
+     * @param req {Request} The express Request object.
+     * @param res {Response} The express Response object.
+     * @next {NextFunction} Execute the next method.
+     */
+    public index(req: Request, res: Response, next: NextFunction) {
+        //set custom title
+        this.title = "Home | Tour of Twitter";
+
+        //set options
+        let options: Object = {
+            "message": "Welcome to the Tour of Twitter",
+            "subtitle": "This is another subtitle"
+        };
+
+        //render template
+        this.render(req, res, "index", options);
+    }
+}
